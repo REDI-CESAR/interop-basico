7db68e7839fbb4e49ce277397077037334b626d3
henriquequeirozcunh <henrique.cunha@corpus.inf.br>
2024-06-29 15:23:56 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { database } from './api/firebase'
+import { onValue, ref } from 'firebase/database'
+
+function App() {
+  const [potenciometro, setPotenciometro] = useState(0);
+  const [luminosidade, setLuminosidade]  = useState(0);
+
+  useEffect(() => {
+    const db = database;
+    const starCountRef = ref(db, 'test');
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log('data', data)
+      setPotenciometro(data.potenciometro);
+      setLuminosidade(data.luminosidade);
+      // updateStarCount(postElement, data);
+    });
+  }, [])
+
+
+  return (
+    <div className="container">
+      <div>
+        Potenciometro: {potenciometro} | Luminosidade: {luminosidade}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/api/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+
+export { database }
